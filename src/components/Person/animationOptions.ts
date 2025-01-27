b35ffd610ecf570f669e6242a5764ad129ed5593
kanarskaya.e <kanarskaya.e@wildberries.work>
2025-01-28 01:11:36 +0300

    fixes - translate3d

--- src/components/Person/animationOptions.ts
+++ src/components/Person/animationOptions.ts
@@ -12,15 +12,15 @@ export const personOptions: KeyframeAnimationOptions = {
 
 export const bodyKeyframes: Keyframe[] | PropertyIndexedKeyframes = [
     {
-        transform: 'translate(-50%, 0%)',
+        transform: 'translate3d(-50%, -63%, 0)',
         offset: 0,
     },
     {
-        transform: 'translate(-50%, -2%)',
+        transform: 'translate3d(-50%, -65%, 0)',
         offset: 0.5,
     },
     {
-        transform: 'translate(-50%, 0%)',
+        transform: 'translate3d(-50%, -63%, 0)',
         offset: 1,
     },
 ];
@@ -34,13 +34,12 @@ export const bodyOptions: KeyframeAnimationOptions = {
 
 export const rightLegKeyframes: Keyframe[] | PropertyIndexedKeyframes = [
     {
-        bottom: '-3%',
-        transform: 'translateX(-62%) rotate(-21deg)',
+        transform: 'translate3d(-62%, 81%, 0) rotate(-21deg)',
         offset: 0,
     },
     {
-        bottom: '-2%',
-        transform: 'translateX(-77%) rotate(19deg)',
+
+        transform: 'translate3d(-62%, 66%, 0) rotate(19deg)',
         offset: 1,
     },
 ];
@@ -55,13 +54,12 @@ export const rightLegOptions: KeyframeAnimationOptions = {
 
 export const leftLegKeyframes: Keyframe[] | PropertyIndexedKeyframes = [
     {
-        bottom: '0%',
-        transform: 'translateX(-74%) rotate(20deg)',
+
+        transform: 'translate3d(-70%, 78%, 0) rotate(20deg)',
         offset: 0,
     },
     {
-        bottom: '0%',
-        transform: 'translateX(-41%) rotate(-21deg)',
+        transform: 'translate3d(-41%, 80%, 0) rotate(-21deg)',
         offset: 1,
     },
 ];
