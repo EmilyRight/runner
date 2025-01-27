cfe6677823c6573becf5e0a96763beec136c6220
kanarskaya.e <kanarskaya.e@wildberries.work>
2025-01-28 01:39:29 +0300

    fixes - useReducer

--- src/components/Person/animationOptions.ts
+++ src/components/Person/animationOptions.ts
@@ -12,15 +12,18 @@ export const personOptions: KeyframeAnimationOptions = {
 
 export const bodyKeyframes: Keyframe[] | PropertyIndexedKeyframes = [
     {
-        transform: 'translate3d(-50%, -63%, 0)',
+        // transform: 'translate3d(-50%, -63%, 0)',
+        transform: 'translate3d(-43px, -74px, 0)',
         offset: 0,
     },
     {
-        transform: 'translate3d(-50%, -65%, 0)',
+        // transform: 'translate3d(-50%, -65%, 0)',
+        transform: 'translate3d(-43px, -71px, 0)',
         offset: 0.5,
     },
     {
-        transform: 'translate3d(-50%, -63%, 0)',
+        // transform: 'translate3d(-50%, -63%, 0)',
+        transform: 'translate3d(-43px, -74px, 0)',
         offset: 1,
     },
 ];
@@ -34,12 +37,13 @@ export const bodyOptions: KeyframeAnimationOptions = {
 
 export const rightLegKeyframes: Keyframe[] | PropertyIndexedKeyframes = [
     {
-        transform: 'translate3d(-62%, 81%, 0) rotate(-21deg)',
+        // transform: 'translate3d(-62%, 81%, 0) rotate(-21deg)',
+        transform: 'translate3d(-22px, 31px, 0) rotate(-21deg)',
         offset: 0,
     },
     {
-
-        transform: 'translate3d(-62%, 66%, 0) rotate(19deg)',
+        // transform: 'translate3d(-62%, 66%, 0) rotate(19deg)',
+        transform: 'translate3d(-24px, 28px, 0) rotate(19deg)',
         offset: 1,
     },
 ];
@@ -54,12 +58,13 @@ export const rightLegOptions: KeyframeAnimationOptions = {
 
 export const leftLegKeyframes: Keyframe[] | PropertyIndexedKeyframes = [
     {
-
-        transform: 'translate3d(-70%, 78%, 0) rotate(20deg)',
+        // transform: 'translate3d(-70%, 78%, 0) rotate(20deg)',
+        transform: 'translate3d(-23px, 30px, 0)  rotate(20deg)',
         offset: 0,
     },
     {
-        transform: 'translate3d(-41%, 80%, 0) rotate(-21deg)',
+        // transform: 'translate3d(-41%, 80%, 0) rotate(-21deg)',
+        transform: 'translate3d(-15px, 34px, 0) rotate(-21deg)',
         offset: 1,
     },
 ];
