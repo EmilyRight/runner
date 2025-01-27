869845a15e068d7094fb8d17b7ce4971094aaa64
kanarskaya.e <kanarskaya.e@wildberries.work>
2025-01-27 15:50:09 +0300

    fixes - width

--- src/game-screens/Game.tsx
+++ src/game-screens/Game.tsx
@@ -7,16 +7,23 @@ import WinScreen from './WinScreen/WinScreen';
 import { OrintationLockModal } from '../components/OrientationModal/orientation-lock-modal';
 import useOrientationChange from '../hooks/use-orientation-change';
 
+enum GameScreenStep {
+    START = 'start',
+    GAME = 'game',
+    GAME_OVER = 'gameOver',
+    WIN = 'win',
+}
+
 function Game() {
     const { screen } = useGameContext();
     const { isLandscape, gameRef } = useOrientationChange();
 
     return (
         <div className='game-screen' ref={gameRef}>
-            {screen === 'start' && <StartScreen />}
-            {screen === 'game' && <GameScreen />}
-            {screen === 'gameOver' && <GameOverScreen />}
-            {screen === 'win' && <WinScreen />}
+            {screen === GameScreenStep.START && <StartScreen />}
+            {screen === GameScreenStep.GAME && <GameScreen />}
+            {screen === GameScreenStep.GAME_OVER && <GameOverScreen />}
+            {screen === GameScreenStep.WIN && <WinScreen />}
             {<OrintationLockModal show={isLandscape} />}
         </div>
     );
