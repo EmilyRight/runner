1f29e9a86b35ef084733fa4f08a0867e22b8573c
kanarskaya.e <kanarskaya.e@wildberries.work>
2025-01-27 22:17:27 +0300

    fixes - translate3d

---
+++ src/game-screens/StartScreen/animationOptions.ts
@@ -0,0 +1,80 @@
+export const backGroundKeyframes: Keyframe[] | PropertyIndexedKeyframes = [
+    {
+        left: '50%',
+        top: '50%',
+        transform: 'translate(-50%, -11%) rotate(0deg)',
+        offset: 0,
+    },
+    {
+        left: '50%',
+        top: '50%',
+        transform: 'translate(-50%, -14%) rotate(0deg)',
+        offset: 0.075,
+    },
+
+    {
+        left: '50%',
+        top: '50%',
+        transform: 'translate(-50%, -11%) rotate(-5deg)',
+        offset: 0.15,
+    },
+
+    {
+        left: '50%',
+        top: '50%',
+        transform: 'translate(-50%, -14%) rotate(0deg)',
+        offset: 0.225,
+    },
+
+    {
+        left: '50%',
+        top: '50%',
+        transform: 'translate(-50%, -11%) rotate(0deg)',
+        offset: 0.3,
+    },
+
+    {
+        left: '50%',
+        top: '50%',
+        transform: 'translate(-50%, -11%) rotate(0deg)',
+        offset: 0.375,
+    },
+
+    {
+        left: '50%',
+        top: '50%',
+        transform: 'translate(-50%, -11%) rotate(0deg)',
+        offset: 0.45,
+    },
+
+    {
+        left: '50%',
+        top: '50%',
+        transform: 'translate(-50%, -11%) rotate(0deg)',
+        offset: 0.525,
+    },
+
+    {
+        left: '50%',
+        top: '50%',
+        transform: 'translate(-50%, -11%) rotate(0deg)',
+        offset: 0.6,
+    },
+
+    {
+        left: '50%',
+        top: '50%',
+        transform: 'translate(-50%, -11%) rotate(0deg)',
+        offset: 0.75,
+    },
+
+
+
+];
+
+export const backGroundOptions: KeyframeAnimationOptions = {
+    duration: 1000,
+    delay: 0,
+    iterations: 1,
+    fill: 'forwards',
+};
